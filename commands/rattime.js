1d7d6e19a8100b958162852cd9dc415bc48f75ce
Joshua Purdy <josh@codesigned.co.uk>
2019-10-27 12:40:20 +0000

    rat time randomised

--- commands/rattime.js
+++ commands/rattime.js
@@ -1,11 +1,35 @@
 module.exports = {
-	name: 'rattime',
+	name: 'rat-time',
 	description: 'Sends rat time image',
 	execute(message, args) {
-        message.channel.send('T I M E  F O R  R A T', {
-		files: [
-            "./assets/rattime.jpg"
-        ]
-        });
+        randInt = Math.random();
+        if (randInt < 0.25){ 
+            message.channel.send('T I M E  F O R  R A T', {
+                files: [
+                    "./assets/rat-time1.jpg"
+                ]
+            });
+        }
+        else if (randInt < 0.50){ 
+            message.channel.send('T I M E  F O R  R A T', {
+                files: [
+                    "./assets/rat-time2.jpg"
+                ]
+            });
+        }
+        else if (randInt < 0.75){ 
+            message.channel.send('T I M E  F O R  R A T', {
+                files: [
+                    "./assets/rat-time3.png"
+                ]
+            });
+        }
+        else{
+            message.channel.send('T I M E  F O R  R A T', {
+                files: [
+                    "./assets/rat-time4.png"
+                ]
+            });
+        }
 	},
 };
