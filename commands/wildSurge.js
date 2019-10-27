51757be16390f8c92a79a8a11688d4fe1e2142ba
Joshua Purdy <josh@codesigned.co.uk>
2019-10-27 20:53:46 +0000

    wildsurge command added

---
+++ commands/wildSurge.js
@@ -0,0 +1,13 @@
+const { wild_values } = require('../assets/wildSurge/table.json');
+
+module.exports = {
+	name: 'wildsurge',
+    description: 'Gives a random wild surge (1 of 10,000)',
+    usage: 'Just send wildsurge, theres really not much to this',
+    example: '',
+	execute(message, args) {
+        randInt = Math.floor((Math.random() * wild_values.length));
+
+        message.channel.send(`You rolled ${randInt}: ${wild_values[randInt]}`);
+	},
+};
