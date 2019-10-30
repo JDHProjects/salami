afe44b1683436d481b336dd7c4d577f00a8047fd
Joshua Purdy <josh@codesigned.co.uk>
2019-10-30 22:34:19 +0000

    rat time images added

--- commands/rattime.js
+++ commands/rattime.js
@@ -4,42 +4,12 @@ module.exports = {
     usage: `Just send rat-time, theres really not much to this`,
     example: '',    
 	execute(message, args) {
-        randInt = Math.floor((Math.random() * 5) + 1);
+        randInt = Math.floor((Math.random() * 13) + 1);
+        message.channel.send('T I M E  F O R  R A T', {
+            files: [
+                "./assets/rat-time/rat-time"+randInt+".png"                
+            ]
+        });
         
-        if (randInt === 1){ 
-            message.channel.send('T I M E  F O R  R A T', {
-                files: [
-                    "./assets/rat-time/rat-time1.jpg"
-                ]
-            });
-        }
-        else if (randInt === 2){ 
-            message.channel.send('T I M E  F O R  R A T', {
-                files: [
-                    "./assets/rat-time/rat-time2.jpg"
-                ]
-            });
-        }
-        else if (randInt === 3){ 
-            message.channel.send('T I M E  F O R  R A T', {
-                files: [
-                    "./assets/rat-time/rat-time3.png"
-                ]
-            });
-        }
-        else if (randInt === 4){ 
-            message.channel.send('T I M E  F O R  R A T', {
-                files: [
-                    "./assets/rat-time/rat-time4.png"
-                ]
-            });
-        }
-        else{
-            message.channel.send('T I M E  F O R  R A T', {
-                files: [
-                    "./assets/rat-time/rat-time5.png"
-                ]
-            });
-        }
 	},
 };
