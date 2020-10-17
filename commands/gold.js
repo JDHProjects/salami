bbfbe388b750add7924adc0295097a37c4cbd07d
Joshua Purdy <josh@codesigned.co.uk>
2020-10-17 18:56:09 +0100

    stat tracking added

--- commands/gold.js
+++ commands/gold.js
@@ -1,4 +1,4 @@
-const wild_values = require('../assets/gold/gold.json');
+const gold_values = require('../assets/gold/gold.json');
 
 module.exports = {
     name: 'gold',
@@ -8,32 +8,32 @@ module.exports = {
 	execute(message, args) {
         var fs = require('fs');
         messageText = ""
-        if(wild_values[message.guild.id] === undefined){
-            wild_values[message.guild.id] = 0
+        if(gold_values[message.guild.id] === undefined){
+            gold_values[message.guild.id] = 0
         }
         if (args[0] != undefined && args[1] != undefined && !isNaN(parseInt(args[1])) ){
             if (args[0].toLowerCase() == "set"){
-                wild_values[message.guild.id] = parseInt(args[1])
+                gold_values[message.guild.id] = parseInt(args[1])
                 messageText += "gold set to " + args[1] + "\n"
                 
             }
             else if (args[0].toLowerCase() == "add"){
-                wild_values[message.guild.id] += parseInt(args[1])
+                gold_values[message.guild.id] += parseInt(args[1])
                 messageText += args[1] + " gold added\n"
             }
             else if (args[0].toLowerCase() == "remove"){
-                wild_values[message.guild.id] -= parseInt(args[1])
+                gold_values[message.guild.id] -= parseInt(args[1])
                 messageText += args[1] + " gold removed\n"
             } 
             
-            fs.writeFile("assets/gold/gold.json", JSON.stringify(wild_values), function(err) {
+            fs.writeFile("assets/gold/gold.json", JSON.stringify(gold_values), function(err) {
                 if (err) {
                     console.log(err);
                 }
             });
         } 
 
-        messageText += "Your total gold is: " + wild_values[message.guild.id]
+        messageText += "Your total gold is: " + gold_values[message.guild.id]
         message.channel.send(messageText)
         
         
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const Discord = require('discord.js')
 const { prefix, token } = require('./config.json');
+const parse = require('csv-parse/lib/sync');
+const stringify = require('csv-stringify/lib/sync');
 const fs = require('fs');
 
 const client = new Discord.Client();
@@ -34,6 +36,53 @@ client.on('message', message => {
         console.error(error);
         message.channel.send('there was an error trying to execute that command!');
     }
+    
+    saveStats(commandName, message.author.id)
+
 });
 
 client.login(token)
+
+function saveStats(commandName, authorId){
+    let data = fs.readFileSync('assets/usage-stats/commands-per-user.csv', 'utf8')
+    statArray = parse(data, {
+        skip_empty_lines: true
+    })
+
+    //append untracked command to array
+    if(!statArray[0].includes(commandName)){
+        for (x in statArray) {
+            if (x == 0){
+                statArray[x].push(commandName)
+            }
+            else{
+                statArray[x].push(0)
+            } 
+        }
+    }
+
+    //get user index
+    let loc = -1
+    for (x in statArray){
+        if(statArray[x][0] == authorId){
+            loc = x
+        }
+    }
+
+    //append untracked user to array
+    if (loc == -1 ){
+        let recordBuilder = []
+        for (x in statArray[0]){
+            recordBuilder.push(0)
+        }
+        recordBuilder[0] = authorId
+        statArray.push(recordBuilder)
+        loc = statArray.length - 1
+    }
+
+    statArray[loc][statArray[0].indexOf(commandName)] += 1
+
+    fs.writeFile('assets/usage-stats/commands-per-user.csv', stringify(statArray, []), function (err) {
+        if (err) throw err;
+      });
+}
