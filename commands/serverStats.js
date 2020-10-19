ff204afa1007f13c6aab04bcf34c65208ec5e79f
Joshua Purdy <josh@codesigned.co.uk>
2020-10-19 21:00:44 +0100

    serverstats added

---
+++ commands/serverStats.js
@@ -0,0 +1,67 @@
+const fs = require('fs');
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+module.exports = {
+    name: 'serverstats',
+    description: 'Gives top commands usage from largest to smallest, defaults to top 3',
+    usage: 'add command name as an arg to get top three users for that command. add arg "full" to get full user list',
+    example: 'ping',
+	execute(message, args) {
+    const { commandStats} = require('../db/dbSetup.js')
+    
+    commandArray = []
+    countArray = []
+    for (const file of commandFiles) {
+      commandArray.push(require(`../commands/${file}`).name)
+      countArray.push(0)
+    }
+
+    buildMsg=""
+
+    full = false
+    command = null
+    for (i in args){
+      if (args[i] == "full"){
+        full = true
+      }
+      else if (commandArray.includes(args[i])){
+        command = args[i]
+      }
+    }
+
+    options = {}
+    if (command != null){
+      options.order = [[command, 'DESC']]
+    }
+
+    
+    commandStats.findAll(options)
+    .then(stats => {
+      for (x in stats){
+        for (i in commandArray){
+          countArray[i] += stats[x].dataValues[commandArray[i]]
+        }
+      }
+      if(command != null){
+        buildMsg+=`**Leaderboard for ${command}**\n`
+        for (x = 0; x < (full ? stats.length : 3); x++) {
+          buildMsg+=`${parseInt(x)+1}) <@${stats[x].dataValues.user_id}> : ${stats[x].dataValues[command]}\n`
+        }
+        total = countArray[commandArray.indexOf(command)]
+      }
+      else{
+        buildMsg+=`**Top used commands**\n`
+        total = countArray.reduce((a, b) => a + b, 0)
+        for (x = 0; x < (full ? commandArray.length : 3); x++) {
+          let maxIndex = countArray.indexOf(Math.max(...countArray))
+          buildMsg+=`${parseInt(x)+1}) ${commandArray[maxIndex]} : ${countArray[maxIndex]}\n`
+          countArray[maxIndex] = -1
+        }
+      }
+      if(full){
+        buildMsg+=`**Total times used: ${total}**\n`
+      }
+      message.channel.send(buildMsg)
+    })
+	},
+};
