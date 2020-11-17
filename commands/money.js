7aac5567bad86eef938b7f0baac58053630cbf9f
Joshua Purdy <josh@codesigned.co.uk>
2020-11-17 20:26:12 +0000

    money can now be transferred

--- commands/money.js
+++ commands/money.js
@@ -1,13 +1,48 @@
 module.exports = {
 	name: 'money',
-  description: 'Tells you your bank account balance',
-  usage: `Just send money, theres really not much to this`,
-  example: '',
+  description: 'Main way to interface with your bank account balance',
+  usage: `With no args it returns your balance, or with args you can send to others`,
+  example: 'send @Salami 10',
 	execute(message, args) {
     const { bankAccounts } = require('../db/dbSetup.js')
+
+    if (message.mentions.users.size > 0){
+     transferUser = message.mentions.users.first()
+    }
+
+    send = false
+    amount = 0
+    for (i in args){
+      if (args[i] == "send"){
+        send = true
+      }
+      else if (!isNaN(parseInt(args[i]))){
+        amount = parseInt(args[i])
+      }
+    }
 		bankAccounts.findByPk(message.author.id)
-    .then(user => {
-      message.channel.send(`<@${message.author.id}>, you have: ${user.dataValues.money} pepperoni${user.dataValues.money>1?"s":""}`)
+    .then(sender => {
+      if (send){
+        if (amount <= sender.dataValues.money){
+          bankAccounts.findByPk(transferUser.id)
+          .then(receiver => {
+            if(receiver != undefined){
+              sender.decrement('money', {by: amount})
+              receiver.increment('money', {by: amount})
+              message.channel.send(`${amount} salami transferred from <@${message.author.id}> to <@${transferUser.id}>`)
+            }
+            else{
+              message.channel.send(`<@${transferUser.id}> doesn't have a bank account, the first time they use the bot, one will be generated for them`)
+            }
+          })
+        }
+        else{
+          message.channel.send(`<@${message.author.id}>, you don't have enough salami`)
+        }
+      }
+      else{
+        message.channel.send(`<@${message.author.id}>, you have: ${sender.dataValues.money} salami`)
+      }
     })
 	},
 };
