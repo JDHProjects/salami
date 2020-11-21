ed98e994591cccafff710cc40aef3c57ba41ba66
Joshua Purdy <josh@codesigned.co.uk>
2020-11-21 14:21:44 +0000

    jackpot costs money now

--- commands/jackpot.js
+++ commands/jackpot.js
@@ -8,19 +8,21 @@ module.exports = {
 
     bankAccounts.findByPk(message.author.id)
     .then(user => {
-      bankAccounts.findByPk( "637400095821660180" )
-      .then(salami => {
-        result = Math.floor(Math.random() * 10000)
-        if(result == 0){
+      result = Math.floor(Math.random() * 10000)
+      if(result == 0){
+        bankAccounts.findByPk( "637400095821660180" )
+        .then(salami => {
           amount = salami.dataValues.money
           message.channel.send(`<@${message.author.id}> **YOU WIN!!!!**\nTransferring ${amount} salami to your account!`)
           user.increment('money', {by: amount})
           salami.decrement('money', {by: amount})
-        }
-        else{
-          message.channel.send(`<@${message.author.id}>, you didn't win the jackpot, better luck next time`)
-        }
-      })
+        })
+      }
+      else{
+        user.decrement('money')
+        message.channel.send(`<@${message.author.id}>, you didn't win the jackpot, better luck next time`)
+      }
+      
     })
 
 		
