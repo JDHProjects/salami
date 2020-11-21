b2fac5b63fb36e7e5acff7a9c7dbefbf7e8a63b5
Joshua Purdy <josh@codesigned.co.uk>
2020-11-21 14:39:00 +0000

    lowered jackpot odds

--- commands/jackpot.js
+++ commands/jackpot.js
@@ -1,6 +1,6 @@
 module.exports = {
 	name: 'jackpot',
-  description: 'Run this command to try and win the jackpot, currently there\'s a 1 in 10,000 chance of winning',
+  description: 'Run this command to try and win the jackpot, currently there\'s a 1 in 5000 chance of winning',
   usage: `just send jackpot`,
   example: '',
 	execute(message, args) {
@@ -11,7 +11,7 @@ module.exports = {
       if(user.dataValues.money >= 2){
         bankAccounts.findByPk( "637400095821660180" )
         .then(salami => {
-          result = Math.floor(Math.random() * 10000)
+          result = Math.floor(Math.random() * 5000)
           if(result == 0){
             amount = salami.dataValues.money
             message.channel.send(`<@${message.author.id}> **YOU WIN!!!!**\nTransferring ${amount} salami to your account!`)
