cc60f184a68c37873922fda12db5302ddfb9cd93
Josh Purdy <j.d.h.purdy@gmail.com>
2021-02-04 19:27:08 +0000

    monkey mimics command added

---
+++ commands/monkeyMimics.js
@@ -0,0 +1,41 @@
+module.exports = {
+	name: 'monkey-mimics',
+    description: 'Sends 3 different images of monkeys to the channel, and one to the user privately. Imitate the noise you think your secret monkey would make and see if anyone can guess which monkey you\'re mimicking',
+    usage: `Just send monkey-mimics, theres really not much to this`,
+    example: '',    
+	execute(message, args) {
+        let photoCount = 257
+        let monkeyOne = 0
+        let monkeyTwo = 0
+        let monkeyThree = 0
+        monkeyOne = Math.floor((Math.random() * photoCount) + 1);
+        do {
+            monkeyTwo = Math.floor((Math.random() * photoCount) + 1);
+        } while (monkeyTwo === monkeyOne)
+        do {
+            monkeyThree = Math.floor((Math.random() * photoCount) + 1);
+        } while (monkeyThree === monkeyOne && monkeyThree === monkeyTwo)
+        message.reply('Lets play Monkey Mimics!');
+        message.channel.send({
+            files: [
+                "./assets/monkey-mimics/"+monkeyOne+".jpeg"
+            ]
+        });
+        message.channel.send({
+            files: [
+                "./assets/monkey-mimics/"+monkeyTwo+".jpeg"
+            ]
+        });
+        message.channel.send({
+            files: [
+                "./assets/monkey-mimics/"+monkeyThree+".jpeg"
+            ]
+        });
+        message.author.send('Heres your monkey to imitate!', {
+            files: [
+                "./assets/monkey-mimics/"+monkeyOne+".jpeg"
+            ]
+        });
+        
+	},
+};
