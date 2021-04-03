89dafd1e71a2a70b7fbcd52eb8dbecfbdd0f43b8
Josh Purdy <j.d.h.purdy@gmail.com>
2021-04-03 20:27:13 +0100

    normalize regular money on restart

--- db/dbSetup.js
+++ db/dbSetup.js
@@ -80,6 +80,9 @@ const sendFromBank = function(user, amount) {
 const transfer = function(sender, reciever, amount) {
 	return new Promise(function(resolve, reject) {
 		let absAmount = Math.abs(Math.round(amount))
+		if (sender.dataValues.money < absAmount){
+			absAmount = sender.dataValues.money
+		}
 		reciever.increment('money', {by: absAmount})
 		sender.decrement('money', {by: absAmount})
 		resolve("money transferred")
@@ -92,7 +95,7 @@ bankAccounts.sum('money')
 .then(total => {
 	bankAccounts.findOrCreate({ where: { user_id: "0" } })
 	.then( bank => {
-		if (total < 1000000000){
+		if (total != 1000000000){
 			bank[0].increment( 'money', { by: 1000000000 - total} )
 		}
 	})
