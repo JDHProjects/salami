699c1053c961f0cb03d75a273d467524da02a6f6
Josh Purdy <j.d.h.purdy@gmail.com>
2021-08-27 22:17:53 +0100

    code duplication removed

--- functions/bulkMessage.js
+++ functions/bulkMessage.js
@@ -2,19 +2,8 @@ const Discord = require('discord.js')
 const channelMessage = new Discord.Collection();
 
 const bulkReply = function(message, messageText) {
-	let replyText = `<@${message.author.id}>, ` + messageText
-	if (!channelMessage.has(message.channel)) {
-		message.channel.startTyping();
-		channelMessage.set(message.channel, replyText);
-		setTimeout(() => {
-			message.channel.stopTyping()
-			message.channel.send(channelMessage.get(message.channel))
-			channelMessage.delete(message.channel)
-		}, 1000);
-	}
-	else{
-		channelMessage.set(message.channel, channelMessage.get(message.channel) + "\n" + replyText);
-	}
+	let replyText = `<@${message.author.id}>, ` + messageText;
+	bulkSend(message, replyText);
 };
 
 const bulkSend = function(message, messageText) {
@@ -22,9 +11,9 @@ const bulkSend = function(message, messageText) {
 		message.channel.startTyping();
 		channelMessage.set(message.channel, messageText);
 		setTimeout(() => {
-			message.channel.stopTyping()
-			message.channel.send(channelMessage.get(message.channel))
-			channelMessage.delete(message.channel)
+			message.channel.stopTyping();
+			message.channel.send(channelMessage.get(message.channel));
+			channelMessage.delete(message.channel);
 		}, 1000);
 	}
 	else{
