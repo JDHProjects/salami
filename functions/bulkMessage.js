7458cb5d8a854598b9582abbbe0aeb7086f51a9d
Josh Purdy <j.d.h.purdy@gmail.com>
2021-08-27 22:07:04 +0100

    typing indicators added to show user the bot is responding despite the 1 second delay

--- functions/bulkMessage.js
+++ functions/bulkMessage.js
@@ -2,11 +2,13 @@ const Discord = require('discord.js')
 const channelMessage = new Discord.Collection();
 
 const bulkReply = function(message, messageText) {
+	
 	let replyText = `<@${message.author.id}>, ` + messageText
 	if (!channelMessage.has(message.channel)) {
+		message.channel.startTyping();
 		channelMessage.set(message.channel, replyText);
 		setTimeout(() => {
-			
+			message.channel.stopTyping()
 			message.channel.send(channelMessage.get(message.channel))
 			channelMessage.delete(message.channel)
 		}, 1000);
@@ -18,9 +20,9 @@ const bulkReply = function(message, messageText) {
 
 const bulkSend = function(message, messageText) {
 	if (!channelMessage.has(message.channel)) {
+		message.channel.sendTyping()
 		channelMessage.set(message.channel, messageText);
 		setTimeout(() => {
-			
 			message.channel.send(channelMessage.get(message.channel))
 			channelMessage.delete(message.channel)
 		}, 1000);
