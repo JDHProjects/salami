12febe03c11b0ce89d26c8a144fe32d433b4cd15
Josh Purdy <j.d.h.purdy@gmail.com>
2021-09-14 13:15:46 +0100

    down detector code moved into its own function

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js')
 const { syncDB } = require('./db/db.js')
 const { runEachCommand } = require('./db/functions/runEachCommand.js')
-const { upOrDown } = require('./db/functions/upOrDown.js')
+const { downDetector } = require('./functions/downDetector.js')
 
 const stringSimilarity = require("string-similarity");
 
@@ -15,23 +15,6 @@ const cooldowns = new Discord.Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 const commandNames = []
 
-const CronJob = require('cron').CronJob;
-var ping = require('ping');
-
-const job = new CronJob('0 * * * * *', function() {
-    ping.promise.probe("8.8.8.8")
-    .then(function (res) {
-        upOrDown(res.alive ? "true" : "false")
-        .then(resp => {
-            if (resp != 0) {
-                client.users.fetch(process.env.OWNER, false).then((admin) => {
-                    admin.send(`Salami was down for: ${Math.round(resp/5)*5} seconds`);
-                });
-            }
-        })
-    })
-});
-
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
 	client.commands.set(command.name, command);
@@ -40,11 +23,7 @@ for (const file of commandFiles) {
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
-
-    if(process.env.DOWN_DETECTION == "TRUE"){
-        job.start();
-        console.log("Down detection started")
-    }
+    downDetector();
 })
 
 client.on('message', message => {
@@ -116,3 +95,5 @@ else {
         client.login()
     })
 }
+
+module.exports = { client }
