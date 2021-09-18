5dcdd85fa94f3e80e802ad963d4041fd3326869d
Josh Purdy <j.d.h.purdy@gmail.com>
2021-09-18 23:46:08 +0100

    circulation command test added

--- commands/circulation.js
+++ commands/circulation.js
@@ -1,4 +1,5 @@
 const { Op } = require("sequelize");
+const { sendMessage } = require('../functions/sendMessage.js')
 
 module.exports = {
 	name: 'circulation',
@@ -6,20 +7,19 @@ module.exports = {
   usage: `just send circulation`,
   example: '',
 	execute(message, args) {
-    const { bankAccounts } = require('../db/db.js')
+    return new Promise(async function(resolve, reject) {
+      const { bankAccounts } = require('../db/db.js')
 
-    bankAccounts.sum('money', { where: { user_id: { [Op.ne]: "0" } } } )
-    .then(total => {
+      let total = await bankAccounts.sum('money', { where: { user_id: { [Op.ne]: "0" } } } )
       let date = new Date();  
       let options = {  
           weekday: "long", year: "numeric", month: "short",  
           day: "numeric", hour: "2-digit", minute: "2-digit"  
       };  
-      bankAccounts.findByPk( "637400095821660180" )
-        .then(salami => {
-          botMoney = salami.dataValues.money
-          message.channel.send(`The current amount of salami in user circulation on ${date.toLocaleTimeString("en-us", options)} is:\n**${total} salami**\n<@637400095821660180> owns ${Math.floor((botMoney/total)*100)}% of the salami in circulation`)
-        })
+      let salami = await bankAccounts.findByPk( "637400095821660180" )
+      botMoney = salami.dataValues.money
+      let messageText = sendMessage(message,`The current amount of salami in user circulation on ${date.toLocaleTimeString("en-us", options)} is:\n**${total} salami**\n<@637400095821660180> owns ${botMoney != 0 ? Math.floor((botMoney/total)*100) : 0}% of the salami in circulation`)
+      resolve(messageText)
     })
 	},
 };
---
+++ test/commands/circulation.js
@@ -0,0 +1,23 @@
+const assert = require('assert');
+
+const { execute } = require('../../commands/circulation.js')
+
+describe('The circulation command', function() {
+  
+  before(async function() {
+    const { refreshBank } = require('../../db/functions/refreshBank.js')
+    const { clearDb } = require('../functions/clearDb.js')
+
+    //sync db and clear tables before each test
+    await clearDb()
+    await refreshBank()
+  })
+
+  it('should respond correctly', async function() {
+    let idealMessage = /The current amount of salami in user circulation on [0-9A-Za-z,: ]+ is:\n\*\*0 salami\*\*\n<@637400095821660180> owns 0% of the salami in circulation/g
+    //The bank currently holds:\n**1000000000 salami**\nWhich is 100% of the total market/
+    let actualMessage = (await execute({}, []))[0]
+    
+    assert.equal(1, (actualMessage.match(idealMessage)).length)
+  })
+})
