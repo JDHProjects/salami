78ff40ba6677736ce2240b4d034d988f93d08369
Josh Purdy <j.d.h.purdy@gmail.com>
2021-09-18 12:10:12 +0100

    formatting changed for test output

--- test/db.js
+++ test/db.js
@@ -3,9 +3,10 @@ const assert = require('assert');
 
 describe('The database', function() {
   it('should successfully create', function() {
+    console.log('\n')
     return syncDB()
     .then(resp => {
-      console.log(resp)
+      console.log(`${resp}\n`)
       assert.equal('Database synced', resp);
     })
   })
