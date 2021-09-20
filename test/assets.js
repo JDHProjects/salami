608362f8d51c22c15b74bbe035e594320051795b
Josh Purdy <j.d.h.purdy@gmail.com>
2021-09-20 16:10:30 +0100

    useless newline characters removed from test output

--- test/assets.js
+++ test/assets.js
@@ -4,31 +4,26 @@ const fs = require('fs');
 describe('The asset folder', function() {
 
   it('should contain at least 3 monkey-mimic images', function() {
-    console.log('\n')
     checkExists('./assets/monkey-mimics/1.jpeg')
     checkExists('./assets/monkey-mimics/2.jpeg');
     checkExists('./assets/monkey-mimics/3.jpeg');
   })
 
   it('should contain the wildsurge data', function() {
-    console.log('\n')
     checkExists('./assets/wildSurge/table.json');
   })
 
   it('should contain the monsters data', function() {
-    console.log('\n')
     checkExists('./assets/monsters/names/names.json');
     checkExists('./assets/monsters/data/data.json');
   })
 
   it('should contain the spells data', function() {
-    console.log('\n')
     checkExists('./assets/spells/names/names.json');
     checkExists('./assets/spells/data/data.json');
   })
 
   it('should contain the items data', function() {
-    console.log('\n')
     checkExists('./assets/items/names/names.json');
     checkExists('./assets/items/data/data.json');
   })
--- test/functions/parseAndRollDice.js
+++ test/functions/parseAndRollDice.js
@@ -6,35 +6,29 @@ const { parseAndRollDice } = require('../../functions/parseAndRollDice')
 describe('The parseAndRollDice function', function() {
 
   it('should return 0 with no input', function() {
-    console.log('\n')
     assert.equal(parseAndRollDice("")[1], 0)
   })
 
   it('should return 0 with invalid input', function() {
-    console.log('\n')
     assert.equal(parseAndRollDice("invalid")[1], 0)
   })
 
   it('should return valid total', function() {
-    console.log('\n')
     let output = parseAndRollDice("1d4 +6 5d2-3")[1]
     assert.ok(output >= 9 && output <= 17)
   })
 
   it('should return integer', function() {
-    console.log('\n')
     let output = parseAndRollDice("1d4")[1]
     assert.ok(!isNaN(output))
   })
 
   it('should return valid total ignoring invalid input', function() {
-    console.log('\n')
     let output = parseAndRollDice("2d5 1d4 +6 5d2-3 -7 +13 d1")[1]
     assert.ok(output >= 18 && output <= 34)
   })
 
   it('should return valid negative number', function() {
-    console.log('\n')
     let output = parseAndRollDice("2d2-7")[1]
     assert.ok(output >= -5 && output <= -3)
   })
