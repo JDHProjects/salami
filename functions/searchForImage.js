ae069bdaffd5879c0bce3921cd2f74e2b42d1ef2
Josh Purdy <j.d.h.purdy@gmail.com>
2021-09-20 22:34:13 +0100

    functions folder linted

--- functions/searchForImage.js
+++ functions/searchForImage.js
@@ -1,19 +1,19 @@
-var gis = require('g-i-s');
+var gis = require("g-i-s")
 
 const searchForImage = function(search, index) {
-	return new Promise(function(resolve, reject) {
-		gis(search, function (error, results) {
-			if (!error && results[0] != undefined) {
-				if (index >= results.length){
-						index = 0;
-				}
-				resolve(results[index].url);
-			}
-			else {
-				reject('Something went wrong, please try again!');
-			}
-		});
-	})
-};
+  return new Promise(function(resolve, reject) {
+    gis(search, function (error, results) {
+      if (!error && results[0] != undefined) {
+        if (index >= results.length){
+          index = 0
+        }
+        resolve(results[index].url)
+      }
+      else {
+        reject("Something went wrong, please try again!")
+      }
+    })
+  })
+}
 
-module.exports = { searchForImage };
+module.exports = { searchForImage }
