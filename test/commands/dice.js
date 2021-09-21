eae866cd07c10b0713f91894a1002aadfd834bbf
Josh Purdy <j.d.h.purdy@gmail.com>
2021-09-21 20:22:11 +0100

    redundent async functions removed

--- test/commands/dice.js
+++ test/commands/dice.js
@@ -4,16 +4,16 @@ const { execute } = require("../../commands/dice.js")
 
 describe("The dice command", function() {
 
-  it("should respond correctly with no input", async function() {
+  it("should respond correctly with no input", function() {
     let idealMessage = "**Overall Total: 0**\n\nOverall Modifier: 0\n**Modifier Adjusted Total: 0**\n\n"
-    let actualMessage = (await execute({}, []))[0]
+    let actualMessage = execute({}, [])[0]
     
     assert.equal(idealMessage, actualMessage)
   })
 
-  it("should respond correctly with an input", async function() {
+  it("should respond correctly with an input", function() {
     let idealMessage = /1 D20: [0-9]+\n\*\*1D20 Total: [0-9]+\*\*\n\n\*\*Overall Total: [0-9]+\*\*\n\nOverall Modifier: \+6\n\*\*Modifier Adjusted Total: [0-9]+\*\*\n\n/g
-    let actualMessage = (await execute({}, ["1d20 +6"]))[0]
+    let actualMessage = execute({}, ["1d20 +6"])[0]
     let matches = actualMessage.match(idealMessage)
     if (matches == null) {
       matches = []
