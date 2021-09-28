2063400aa5d47630169e0578801da52b79e66f2a
Josh Purdy <j.d.h.purdy@gmail.com>
2021-09-28 17:38:12 +0100

    more commands converted to buttons

--- commands/coinflip.js
+++ commands/coinflip.js
@@ -1,4 +1,5 @@
 const { sendMessage } = require("../functions/sendMessage.js")
+const { getButtonRow } = require("../functions/getButtonRow.js")
 
 module.exports = {
   name: "coinflip",
@@ -12,45 +13,58 @@ module.exports = {
     const { lossWithTax } = require("../db/functions/lossWithTax.js")
     const { sendFromBank } = require("../db/functions/sendFromBank.js")
 
+    const numToText = ["Heads", "Tails"]
     let messageText = ""
+    let user = null
 
-    let amount = 0
-    let guess = -1
-    for (let i in args){
-      if (args[i] == "heads"){
-        guess=1
-      }
-      else if (args[i] == "tails"){
-        guess=0
-      }
-      else if (!isNaN(parseInt(args[i]))){
-        amount = Math.abs(parseInt(args[i]))
+    let amount = 0 
+    if (args.length > 0){
+      if (!isNaN(parseInt(args[0]))){
+        amount = Math.abs(parseInt(args[0]))
       }
     }
 
-    if(guess != -1){
-      let user = await bankAccounts.findByPk(message.author.id)
-      if (amount <= user.dataValues.money){
-        let flip = Math.floor(Math.random() * 2) 
-        if(flip == guess){
-          messageText = await sendMessage.send(message,`${flip == 1 ? "heads" : "tails"}! <@${message.author.id}>, you win!`)
-          if (amount > 0){
-            sendFromBank(user, amount)
-          }
-        }
-        else{
-          messageText = await sendMessage.send(message,`${flip == 1 ? "heads" : "tails"}! <@${message.author.id}>, you lose`)
-          if (amount > 0){
-            lossWithTax(user, amount)
-          }
-        }
+    if(amount != 0){
+      user = await bankAccounts.findByPk(message.author.id)
+      if (amount > user.dataValues.money){
+        return await sendMessage.reply(message,"you don't have enough salami to make that bet")
       }
-      else{
-        messageText = await sendMessage.reply(message,"you don't have enough salami to make that bet")
+    }
+
+    let userGuess = -1
+
+    const filter = (m) => m.user.id === message.author.id
+
+    let question = await sendMessage.reply(message, "Please select an option", { components: [getButtonRow({ buttons: [{ name:"Heads", id:"heads" },{ name:"Tails", id:"tails" }] })] })
+
+    try {
+      let buttonResp = await question.awaitMessageComponent({ filter, time: 5000 })
+    
+      if (buttonResp.customId === "heads") {
+        await buttonResp.update({ content: "Selection made!", components: [getButtonRow({ disabled:true, buttons: [{ name:"Heads", id:"heads", picked:true },{ name:"Tails", id:"tails" }] })] })
+        userGuess = 0
+      }
+      else if (buttonResp.customId === "tails") {
+        await buttonResp.update({ content: "Selection made!", components: [getButtonRow({ disabled:true, buttons: [{ name:"Heads", id:"heads" },{ name:"Tails", id:"tails", picked:true }] })] })
+        userGuess = 1
+      }
+    } catch {
+      return await sendMessage.edit(question, "You forgot to pick an option :(", { components: [getButtonRow({ disabled:true, buttons: [{ name:"Heads", id:"heads" },{ name:"Tails", id:"tails" }] })] })
+    }
+
+    
+    let flip = Math.floor(Math.random() * 2) 
+    if(flip == userGuess){
+      messageText = await sendMessage.reply(message,`${numToText[flip]}, you win!`)
+      if (amount > 0){
+        await sendFromBank(user, amount)
       }
     }
     else{
-      messageText = await sendMessage.send(message,"You forgot to pick heads or tails!")
+      messageText = await sendMessage.reply(message,`${numToText[flip]}, you lose`)
+      if (amount > 0){
+        await lossWithTax(user, amount)
+      }
     }
     return messageText
   },
--- test/commands/rockPaperScissors.js
+++ test/commands/rockPaperScissors.js
@@ -1,11 +1,11 @@
 const assert = require("assert")
 
-const { execute } = require("../../commands/dicegame.js")
+const { execute } = require("../../commands/rockPaperScissors.js")
 
 const { dummyMessage } = require("../helpers/dummyMessages.js")
 const { setUserMoney } = require("../helpers/setUserMoney.js")
 
-describe("The dicegame command", function() {
+describe("The rps command", function() {
   it("should respond correctly when user has insufficient money", async function() {
     const { refreshBank } = require("../../db/functions/refreshBank.js")
     const { clearDb } = require("../helpers/clearDb.js")
@@ -17,7 +17,7 @@ describe("The dicegame command", function() {
 
     let idealMessage = "you don't have enough salami to make that bet"
     let actualMessage = (await execute(dummyMessage, ["100"])).content
-    
+
     assert.equal(idealMessage, actualMessage)
   })
 })
